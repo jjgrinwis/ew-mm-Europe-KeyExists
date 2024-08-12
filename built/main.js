b33574ee9452e2a70d46f303f6d7250719d14c07
John Grinwis <john@grinwis.com>
2024-08-12 22:50:27 +0200

    extra validation on bindVars added

--- built/main.js
+++ built/main.js
@@ -11,26 +11,39 @@ export async function responseProvider(request) {
     */
     const authorizationHeader = `apikey ${request.getVariable("PMUSER_MM_APIKEY")}`;
     let result = {};
-    try {
-        const document = await executeRestQL(QUERY_WORKER_NAME, authorizationHeader, JSON.parse(params.get("bindVars")));
-        // if executeRestQL succeeds, just set status to 200 and assign response to the result
-        logger.info("Macrometa Query Worker call succeeded");
-        status = 200;
-        result = document;
-    }
-    catch (error) {
-        // something went wrong
-        logger.error("Error occurred while executing edgeWorker", error.toString());
-        status = error.status || 500;
+    // check if bindVars is provided.
+    const bindVars = params.get("bindVars");
+    // if for bindVars is not provided, create error result
+    // during testing we found out that providing no value will result in a true with our current Query Worker
+    // need to fix that, someday.
+    if (!bindVars || bindVars === "null") {
+        result.errorCode = 400;
+        result.errorMessage = "no bindVar query string provided";
         result.error = true;
-        if (error.status) {
-            const dbError = JSON.parse(error.message) || {};
-            result.errorCode = dbError.code;
-            result.errorMessage = dbError.errorMessage || "Something went wrong";
-            result.errorNum = dbError.errorNum;
+        status = 200;
+    }
+    else {
+        try {
+            const document = await executeRestQL(QUERY_WORKER_NAME, authorizationHeader, JSON.parse(bindVars));
+            // if executeRestQL succeeds, just set status to 200 and assign response to the result
+            logger.info("Macrometa Query Worker call succeeded");
+            status = 200;
+            result = document;
         }
-        else {
-            result.errorMessage = error.toString() || "Something went wrong";
+        catch (error) {
+            // something went wrong
+            logger.error("Error occurred while executing edgeWorker", error.toString());
+            status = error.status || 500;
+            result.error = true;
+            if (error.status) {
+                const dbError = JSON.parse(error.message) || {};
+                result.errorCode = dbError.code;
+                result.errorMessage = dbError.errorMessage || "Something went wrong";
+                result.errorNum = dbError.errorNum;
+            }
+            else {
+                result.errorMessage = error.toString() || "Something went wrong";
+            }
         }
     }
     return Promise.resolve(createResponse(status, {
--- main.ts
+++ main.ts
@@ -26,30 +26,46 @@ export async function responseProvider(request: EW.ResponseProviderRequest) {
 
   let result: QueryWorkerErrorResult = {};
 
-  try {
-    const document = await executeRestQL(
-      QUERY_WORKER_NAME,
-      authorizationHeader,
-      JSON.parse(params.get("bindVars"))
-    );
+  // check if bindVars is provided.
+  const bindVars = params.get("bindVars");
 
-    // if executeRestQL succeeds, just set status to 200 and assign response to the result
-    logger.info("Macrometa Query Worker call succeeded");
-    status = 200;
-    result = document;
-  } catch (error) {
-    // something went wrong
-    logger.error("Error occurred while executing edgeWorker", error.toString());
-    status = error.status || 500;
+  // if for bindVars is not provided, create error result
+  // during testing we found out that providing no value will result in a true with our current Query Worker
+  // need to fix that, someday.
+  if (!bindVars || bindVars === "null") {
+    result.errorCode = 400;
+    result.errorMessage = "no bindVar query string provided";
     result.error = true;
+    status = 200;
+  } else {
+    try {
+      const document = await executeRestQL(
+        QUERY_WORKER_NAME,
+        authorizationHeader,
+        JSON.parse(bindVars)
+      );
 
-    if (error.status) {
-      const dbError = JSON.parse(error.message) || {};
-      result.errorCode = dbError.code;
-      result.errorMessage = dbError.errorMessage || "Something went wrong";
-      result.errorNum = dbError.errorNum;
-    } else {
-      result.errorMessage = error.toString() || "Something went wrong";
+      // if executeRestQL succeeds, just set status to 200 and assign response to the result
+      logger.info("Macrometa Query Worker call succeeded");
+      status = 200;
+      result = document;
+    } catch (error) {
+      // something went wrong
+      logger.error(
+        "Error occurred while executing edgeWorker",
+        error.toString()
+      );
+      status = error.status || 500;
+      result.error = true;
+
+      if (error.status) {
+        const dbError = JSON.parse(error.message) || {};
+        result.errorCode = dbError.code;
+        result.errorMessage = dbError.errorMessage || "Something went wrong";
+        result.errorNum = dbError.errorNum;
+      } else {
+        result.errorMessage = error.toString() || "Something went wrong";
+      }
     }
   }
 
